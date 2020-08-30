ad44b23e993afa691b61f86b7862b063db95636e
Storiwr <storiwr@users.noreply.github.com>
2020-08-30 15:10:25 -0400

    Initial Commit

---
+++ CardStack/CardStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardStackApp.swift
+//  CardStack
+//
+//  Created by Stephen on 8/30/20.
+//
+
+import SwiftUI
+
+@main
+struct CardStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
