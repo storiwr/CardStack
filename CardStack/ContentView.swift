18879ce5838393dc58936312dccd4416ed5098bf
Storiwr <storiwr@users.noreply.github.com>
2020-09-03 17:28:08 -0400

    Ugh! Took forever to realize used wrong struct name in new screen. Everything is functional now so yay!

--- CardStack/ContentView.swift
+++ CardStack/ContentView.swift
@@ -6,63 +6,99 @@
 
 import SwiftUI
 
-class CardData: ObservableObject {
-    @Published var name = "Hello Card"
-    @Published var cardBody = "I'm so happy to be here!"
+struct CardInfo: Codable, Identifiable {
+    let id = UUID()
+    let name: String
+    let cardBody: String
+//    let amount: Int
+}
+
+class Cards: ObservableObject {
+    @Published var items: [CardInfo] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([CardInfo].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+
+        self.items = []
+    }
 }
 
 struct ContentView: View {
-    @ObservedObject private var card = CardData()
-    @State private var showingSheet = false
+    @ObservedObject var cards = Cards()
+    @State private var showingNewCardView = false
     var body: some View {
         NavigationView {
             List {
-                Text(card.name)
-                Text(card.cardBody)
+                ForEach(cards.items) { card in
+                    VStack {
+                        Text(card.name)
+                        Text(card.cardBody)
+                    }
+                    
+                }.onDelete(perform: removeItems)
                 
             }.navigationBarTitle("Cards")
-                .navigationBarItems(trailing:
-                                        Button("New"){
-                                            self.showingSheet.toggle()
-                                        } .sheet(isPresented: $showingSheet, content: {
-                                            NewView()
-                                        }))
-        }
-}
-
-
-struct DetailView: View {
-    @ObservedObject private var card = CardData()
-    var body: some View {
-        NavigationView {
-            List {
-                Text(card.cardBody)
-                Text(card.name)
-            }.navigationBarTitle(Text("Card Title"))
-            .navigationBarItems(trailing: EditButton())
-            
-        }
+                    .navigationBarItems(trailing:
+                        Button(action: {
+                            self.showingNewCardView = true
+                        }) {
+                            Image(systemName: "plus")
+                        }
+                    )
+                    .sheet(isPresented: $showingNewCardView) {
+                        NewCardView(cards: self.cards)
+                    }
+                }
+            }
+    func removeItems(at offsets: IndexSet) {
+        cards.items.remove(atOffsets: offsets)
     }
 }
+
+//struct DetailView: View {
+//    @ObservedObject private var card = CardInfo()
+//    var body: some View {
+//        NavigationView {
+//            List {
+//                Text(card.cardBody)
+//                Text(card.name)
+//            }.navigationBarTitle(Text("Card Title"))
+//            .navigationBarItems(trailing: EditButton())
+//
+//        }
+//    }
+//}
     
-    struct NewView: View {
-        @ObservedObject private var card = CardData()
-        @Environment(\.presentationMode) var presentationMode
-        
-        var body: some View {
-            NavigationView {
-                List {
-                    TextField("Title", text: $card.name)
-                    TextField("Body", text: $card.cardBody)
-                }.navigationBarTitle(Text("NewCard"))
-                .navigationBarItems(trailing:
-                                        Button("Done") {
-                                            self.presentationMode.wrappedValue.dismiss()
-                                        }
-                
-            )}
-        }
-    }
+//    struct NewView: View {
+//        @ObservedObject private var card = CardInfo()
+//        @Environment(\.presentationMode) var presentationMode
+//
+//        var body: some View {
+//            NavigationView {
+//                List {
+//                    TextField("Title", text: $card.name)
+//                    TextField("Body", text: $card.cardBody)
+//                }.navigationBarTitle(Text("NewCard"))
+//                .navigationBarItems(trailing:
+//                                        Button("Done") {
+//                                            self.presentationMode.wrappedValue.dismiss()
+//                                        }
+//
+//            )}
+//        }
+//    }
 
     
 struct ContentView_Previews: PreviewProvider {
@@ -70,4 +106,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-}
+
---
+++ CardStack/NewCardView.swift
@@ -0,0 +1,41 @@
+//
+//  NewCardView.swift
+//  CardStack
+//
+//  Created by Stephen on 9/3/20.
+//
+
+import SwiftUI
+
+struct NewCardView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var cards: Cards
+    @State private var name = ""
+    @State private var cardBody = ""
+//    @State private var amount = ""
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Title", text: $name)
+                TextField("Body", text: $cardBody)
+
+                }
+                .navigationBarTitle("New Card")
+                .navigationBarItems(trailing: Button("Done") {
+//                if let actualAmount = Int(self.amount) {
+                let item = CardInfo(name: self.name, cardBody: self.cardBody)
+                    self.cards.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
+//            }
+        })
+    }
+    }
+}
+
+
+struct NewCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewCardView(cards: Cards())
+    }
+}
